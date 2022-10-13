ec4f381bbde30f1baab62cbfe532eea9d7f774bc
justin <justin@modusense.com>
2022-10-13 17:58:30 +1300

    created ingredients component

--- src/App.js
+++ src/App.js
@@ -13,14 +13,38 @@ const sampleRecipes = [
       name: 'Plain Chicken',
       servings: 3,
       cookTime: '1:45',
-      instructions: "1. put salt\n2. put chicken in oven\n3. eat the chicken"
+      instructions: "1. put salt\n2. put chicken in oven\n3. eat the chicken",
+      ingredients:[
+        {
+          id:1,
+          name: 'Chicken',
+          amount: '2 Pounds'
+        },
+        {
+          id:2,
+          name: 'Salt',
+          amount: '1 Tbs'
+        }
+      ]
   },
   {
       id: 2,
       name: 'Plain Pork',
       servings: 5,
       cookTime: '2:45',
-      instructions: "1. put salt\n2. put pork in oven\n3. eat the pork"
+      instructions: "1. put salt\n2. put pork in oven\n3. eat the pork",
+      ingredients:[
+        {
+          id:1,
+          name: 'Pork',
+          amount: '5 Pounds'
+        },
+        {
+          id:2,
+          name: 'Paprika',
+          amount: '3 Tbs'
+        }
+      ]
   },
 ]
 
