f261d8981afcb4f20c2350c1a82af8775ef84bd8
justin <justin@modusense.com>
2022-10-18 16:59:08 +1300

    finished recipe and ingredient css

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -12,31 +12,31 @@ const Recipe = (props) => {
   } = props;
     
   return (
-    <div>
-      <div>
-        <h3>{name}</h3>
+    <div className='recipe'>
+      <div className='recipe__header'>
+        <h3 className='recipe__title'>{name}</h3>
         <div>
-            <button>Edit</button>
-            <button>Delete</button>
+            <button className='btn btn--primary'>Edit</button>
+            <button className='btn btn--danger'>Delete</button>
         </div>
       </div>
-      <div>
-        <span>Cook time:</span>
-        <span>{cookTime}</span>
+      <div className='recipe__row'>
+        <span className='recipe__label'>Cook time:</span>
+        <span className='recipe__value'>{cookTime}</span>
       </div>
-      <div>
-        <span>Serving:</span>
-        <span>{servings}</span>
+      <div className='recipe__row'>
+        <span className='recipe__label'>Serving:</span>
+        <span className='recipe__value'>{servings}</span>
       </div>
-      <div>
-        <span>Instruction:</span>
-        <div>
+      <div className='recipe__row'>
+        <span className='recipe__label'>Instruction:</span>
+        <div className='recipe__value recipe__instructions recipe__value--indented'>
             {instructions}
         </div>
       </div>
-      <div>
-        <span>Ingredients:</span>
-        <div>
+      <div className='recipe__row'>
+        <span className='recipe__label'>Ingredients:</span>
+        <div className='recipe__value recipe__value--indented'>
             <IngredientList ingredients={ingredients} />
         </div>
       </div>
