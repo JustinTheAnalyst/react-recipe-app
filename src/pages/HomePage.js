0cd358539a51d1c20debfbf84d920a8c00b36fe7
justin <justin@modusense.com>
2022-10-18 17:47:22 +1300

    added handle recipe

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageHeader from '../components/PageHeader';
 import RecipeList from '../components/RecipeList';
-
+import { v4 as uuidv4 } from 'uuid';
 
 const HomePage = () => {
+  const [recipes, setRecipes] = useState(sampleRecipes);
+
+  function handleRecipeAdd(){
+    const newRecipe = {
+      id: uuidv4(),
+      name: 'New',
+      servings: 1,
+      cookTime: '1:00',
+      instructions: 'Instr.',
+      ingredients: [
+        {id: uuidv4(), name: 'Name', amount:'1 Tbsp'}
+      ]
+    }
+  
+    setRecipes([...recipes, newRecipe]);
+  }
+
   return (
     <>
       <PageHeader title="Homepage" />
-      <RecipeList recipes={sampleRecipes} />
+      <RecipeList 
+        recipes={recipes} 
+        handleRecipeAdd={handleRecipeAdd}
+      />
     </>
   )
+  
 }
+
+
 
 const sampleRecipes = [
   {
